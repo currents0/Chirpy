f61f84c8dbd0e7d65dd2377ab4e672d9a3f253b8
currents0 <legendofzeldanerd49@gmail.com>
2024-07-12 17:18:13 -0400

    added index and img

--- main.go
+++ main.go
@@ -5,5 +5,6 @@ import "net/http"
 func main(){
 	serveMux := http.NewServeMux()
 	server := &http.Server{Addr: "localhost:8080", Handler: serveMux}
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
 	server.ListenAndServe()
 }
