942ec513165373daafeb44910437faa0660a696a
currents0 <legendofzeldanerd49@gmail.com>
2024-07-12 17:59:07 -0400

    updated server

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import "net/http"
 func main(){
 	serveMux := http.NewServeMux()
 	server := &http.Server{Addr: "localhost:8080", Handler: serveMux}
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request){
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(http.StatusText(http.StatusOK)))
+	})
 	server.ListenAndServe()
 }
